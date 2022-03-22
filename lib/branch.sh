0585dc07d1ecb64e1932c5d2d099d317148008cc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-21 20:25:10 -0400

    ci: testing

--- lib/branch.sh
+++ lib/branch.sh
@@ -14,10 +14,20 @@
 p6_github_branch_transliterate() {
     local msg="$1"
 
+    local suffix
+    suffix=$(p6_token_random "5")
+
+    local prefix="p6df-"
+
+    local kind
+    kind=$(p6_echo "$msg" | cut -d : -f 1)
+
+    local rest
+    rest=$(p6_echo "$msg" | cut -d : -f 2 | sed -e 's,^ *,,' -e 's, ,_,g')
+    rest=$(p6_string_replace "$rest" "[^A-Za-z0-9_#]" "")
+
     local branch
-    branch=$(p6_string_replace "$msg" ":" "#")
-    branch=$(p6_string_replace "$branch" " " "_")
-    branch=$(p6_string_replace "$branch" "[^A-Za-z0-9_#]" "")
+    branch="$kind/$rest#$prefix$suffix"
 
     p6_return_str "$branch"
 }
